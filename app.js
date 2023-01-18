da75c3c17d4228b7605b5731233f5bea85727c9f
MeyAdam <meyadam11@gmail.com>
2023-01-18 20:49:54 +0800

    comment added for better understanding

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ const rules2needs = document.querySelector('.rules2-needs');
 const rules2wants = document.querySelector('.rules2-wants');
 const rules2saves = document.querySelector('.rules2-saves');
 
+// input event listener so the result is shown whilst the user still typing
 income.addEventListener('input', () => {
+    //by default the div is display none and will show once user insert input
     rulesDiv.style.display = 'grid';
     incomeaftertax = income.value;
 
+    // toFixed(2) to round of long decimal and only show two decimal places
     rules1needs.textContent = 'RM ' + (incomeaftertax * 0.5).toFixed(2);
     rules1wants.textContent = 'RM ' + (incomeaftertax * 0.3).toFixed(2);
     rules1saves.textContent = 'RM ' + (incomeaftertax * 0.2).toFixed(2);
@@ -23,6 +26,7 @@ income.addEventListener('input', () => {
     rules2wants.textContent = 'RM ' + (incomeaftertax * 0.2).toFixed(2);
     rules2saves.textContent = 'RM ' + (incomeaftertax * 0.1).toFixed(2);
 
+    // if input == '', rulesDiv style is removed
     if (incomeaftertax == '') {
         rulesDiv.removeAttribute('style');
     }
