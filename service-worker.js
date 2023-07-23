adc71b2b3d34a6720e7e3000aba3491b7f098fad
MeyAdam <meyadam11@gmail.com>
2023-07-23 14:41:29 +0800

    convert to pwa

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+// Define a unique cache name
+const cacheName = 'bajetgaji-cache-v2';
+
+// List the files to cache
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/main.js',
+  '/assets/js/language.js',
+  '/images/icon-72x72.png',
+  '/images/icon-96x96.png',
+  '/images/icon-144x144.png',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        console.log('Cache opened');
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+        // if (response) {
+        //   return response;
+        // }
+        // return fetch(event.request);
+      })
+  );
+});
